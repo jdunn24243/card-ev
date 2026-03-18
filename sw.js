8c1a66c30fc570a098ffac273146e421748ef321
jdunn24243 <delatus2002@gmail.com>
2026-03-17 20:49:44 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
 const CACHE = 'card-ev-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
-
+const ASSETS = ['/card-ev/', '/card-ev/index.html', '/card-ev/manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
